7d027ad13a449a2c7dfddcbbeacdd5566f82afba
Ethan Thompson <ethanlt6443.pie@gmail.com>
2026-06-26 22:19:40 -0500

    began car object

---
+++ car.py
@@ -0,0 +1,18 @@
+# ========================= #
+#         Car Class         #
+# ========================= #
+# imports
+
+
+class Car:
+    def __init__(self):
+        return
+
+    def idle_state(self):
+        return
+    
+    def armed(self):
+        return
+    
+    def running(self):
+        return
